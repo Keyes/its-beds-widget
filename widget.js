14f6821dcb9c50dc1b116d8c50ca62b2b8678e25
Julian Kern <mail@juliankern.com>
2020-10-24 20:32:27 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -5,73 +5,74 @@ const getApiUrl = (location) => {
   return apiUrlBase;
 }
 
-const widget = await createWidget()
+const widget = await createWidget();
 if (!config.runsInWidget) {
-  await widget.presentSmall()
+  await widget.presentSmall();
 }
-Script.setWidget(widget)
-Script.complete()
+
+Script.setWidget(widget);
+Script.complete();
 
 async function createWidget(items) {
-  const data = await getData()
-  const list = new ListWidget()
-  const header = list.addText("🛏 Freie Intensivbetten")
-  header.font = Font.mediumSystemFont(12)
+  const data = await getData();
+  const list = new ListWidget();
+  const header = list.addText("🛏 Freie Intensivbetten");
+  header.font = Font.mediumSystemFont(12);
 
   if (data) {
-    list.addSpacer()
+    list.addSpacer();
 
     if (data.state) {
-      const label = list.addText(data.state.used.toFixed(2) + "%")
-      label.font = Font.mediumSystemFont(22)
-      label.textColor = data.state.used <= 25 ? Color.red() : data.state.used <= 50 ? Color.orange() : Color.green()
+      const label = list.addText(data.state.used.toFixed(2) + "%");
+      label.font = Font.mediumSystemFont(22);
+      label.textColor = data.state.used <= 25 ? Color.red() : data.state.used <= 50 ? Color.orange() : Color.green();
 
-      const location = list.addText(data.state.name)
-      location.font = Font.lightSystemFont(12)
-      list.addSpacer(4)
+      const location = list.addText(data.state.name);
+      location.font = Font.lightSystemFont(12);
+      list.addSpacer(4);
     }
 
-    const label = list.addText(data.overall.used.toFixed(2) + "%")
-    label.font = Font.mediumSystemFont(22)
-    label.textColor = data.overall.used <= 25 ? Color.red() : data.overall.used <= 50 ? Color.orange() : Color.green()
-    const location = list.addText('Deutschland')
-    location.font = Font.lightSystemFont(12)
-    list.refreshAfterDate = new Date(Date.now() + 60 * 60 * 1000)
+    const label = list.addText(data.overall.used.toFixed(2) + "%");
+    label.font = Font.mediumSystemFont(22);
+    label.textColor = data.overall.used <= 25 ? Color.red() : data.overall.used <= 50 ? Color.orange() : Color.green();
+    const location = list.addText('Deutschland');
+    location.font = Font.lightSystemFont(12);
+    list.refreshAfterDate = new Date(Date.now() + 60 * 60 * 1000);
 
-    list.addSpacer(6)
+    list.addSpacer(6);
     const dateFormatter = new DateFormatter();
-    dateFormatter.useShortDateStyle()
-    dateFormatter.useShortTimeStyle()
+    dateFormatter.useShortDateStyle();
+    dateFormatter.useShortTimeStyle();
 
-    const updated = list.addText(`↻ ${dateFormatter.string(new Date(data.overall.updated))}`)
-    updated.font = Font.lightSystemFont(9)
+    const updated = list.addText(`↻ ${dateFormatter.string(new Date(data.overall.updated))}`);
+    updated.font = Font.lightSystemFont(9);
   } else {
-    list.addSpacer()
-    list.addText("Daten nicht verfügbar")
+    list.addSpacer();
+    list.addText("Daten nicht verfügbar");
   }
 
-  return list
+  return list;
 }
 
 async function getData() {
   try {
-    const location = await getLocation()
+    const location = await getLocation();
     const foundData = await new Request(getApiUrl(location)).loadJSON();
 
     return foundData;
   } catch (e) {
-    return null
+    return null;
   }
 }
 
 async function getLocation() {
   try {
     if (args.widgetParameter) {
-      const fixedCoordinates = args.widgetParameter.split(",").map(parseFloat)
-      return { latitude: fixedCoordinates[0], longitude: fixedCoordinates[1] }
+      const fixedCoordinates = args.widgetParameter.split(",").map(parseFloat);
+      return { latitude: fixedCoordinates[0], longitude: fixedCoordinates[1] };
     } else {
-      Location.setAccuracyToThreeKilometers()
-      return await Location.current()
+      Location.setAccuracyToThreeKilometers();
+      return await Location.current();
     }
   } catch (e) {
     return null;
