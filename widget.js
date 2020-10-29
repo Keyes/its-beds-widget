baaebb284cbbd5321cc790490042f477c9cea6a4
Julian Kern <mail@juliankern.com>
2020-10-29 02:34:15 +0100

    thinner arrow

--- widget.js
+++ widget.js
@@ -145,7 +145,7 @@ function renderDatablock(list, data, weekData) {
   
     const trendIconImage = percentLabel.addImage(trendIcon.image);
     trendIconImage.tintColor = getPercentageColor(data.used);
-    trendIconImage.imageSize = new Size(12, 15);
+    trendIconImage.imageSize = new Size(10, 15);
     trendIconImage.resizeable = false;
   }
 
